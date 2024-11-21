f7ebe4fce3611f55609d4dbae6d8dba60cd59375
aluno <aluno@PC-Professor>
2024-11-21 20:44:34 -0300

    Adicionado exemplos dde métodos da class math.

---
+++ modulo5/src/modulo5/Exmath.java
@@ -0,0 +1,21 @@
+package modulo5;
+
+public class Exmath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 	System.out.printf("Resultado do método sin(): %.2f\n", Math.sin(10.5));
+		    System.out.printf("Resultado do método cos(): %.2f\n", Math.cos(10.5));
+		    System.out.printf("Resultado do método tan(): %.2f\n", Math.tan(10.5));
+		    System.out.printf("Resultado do método floor(): %.2f\n", Math.floor(10.5));
+		    System.out.printf("Resultado do método ceil(): %.2f\n", Math.ceil(10.5));
+		    System.out.printf("Resultado do método abs(): %.2f\n", Math.abs(-10.5));
+		    System.out.printf("Resultado do método sqrt(): %.2f\n", Math.sqrt(9));
+		    System.out.printf("Resultado do método pow(): %.2f\n", Math.pow(2, 5));    
+		    System.out.printf("Resultado do método random(): %.2f\n", Math.random());
+		    
+	}
+
+}
+
+
